dd693348fc5127571081f07358415178631ef328
Vlady Veselinov <vladkodmc@gmail.com>
2025-01-30 11:39:31 +0000

    edit comment

--- app/routes/auth/auth-server.ts
+++ app/routes/auth/auth-server.ts
@@ -36,7 +36,7 @@ export function createAuthServer({
   //     ),
   //   },
   //   theme: {
-  //     title: "G-Yarns",
+  //     title: "My App",
   //     primary: "#0051c3",
   //     favicon: "https://workers.cloudflare.com//favicon.ico",
   //     logo: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNoaXJ0Ij48cGF0aCBkPSJNMjAuMzggMy40NiAxNiAyYTQgNCAwIDAgMS04IDBMMy42MiAzLjQ2YTIgMiAwIDAgMC0xLjM0IDIuMjNsLjU4IDMuNDdhMSAxIDAgMCAwIC45OS44NEg2djEwYzAgMS4xLjkgMiAyIDJoOGEyIDIgMCAwIDAgMi0yVjEwaDIuMTVhMSAxIDAgMCAwIC45OS0uODRsLjU4LTMuNDdhMiAyIDAgMCAwLTEuMzQtMi4yM3oiLz48L3N2Zz4=",
